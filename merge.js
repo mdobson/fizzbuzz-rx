92809515b34a60ce3667bcff787e1b1601376d99
Matt Dobson <mdobson4@gmail.com>
2014-07-11 11:12:13 -0400

    Added more observables to the merged stream.

--- merge.js
+++ merge.js
@@ -10,11 +10,17 @@ var generic = Rx.Observable.fromEvent(e, 'test');
 var created = Rx.Observable.create(function(observer) {
   setInterval(function() {
     observer.onNext(42);
-  }, 2000);
+  }, 500);
+});
+var ends = Rx.Observable.create(function(observer) {
+  var a = [1, 2, 3];
+  a.forEach(function(i) {
+    observer.onNext(i);
+  });
+  observer.onCompleted();
 });
 
-
-var source = Rx.Observable.merge(fizzbuzz, generic, created);
+var source = Rx.Observable.merge(fizzbuzz, generic, created, ends);
 
 var sub = source.subscribe(function(x) {
   console.log(x);
